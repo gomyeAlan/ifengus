489564b9fdee767ca047f31b733994ad9818cd43
wwwendydev <us68design@gmail.com>
2021-02-02 13:33:29 -0800

    wendy change archives data 20200202

--- ifengus_v20/ViewModel/ListManager.swift
+++ ifengus_v20/ViewModel/ListManager.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+struct isresponsearchives: Codable {
+    let code: Int
+    let msg: String
+    let time: String
+    let data: [Lists]?
+}
+
 class ListManager :ObservableObject  {
     
     //建立一个集合
@@ -19,25 +26,25 @@ class ListManager :ObservableObject  {
     }
     
     func getListContent(pid:Int) {
-
-           let url = URL(string: "https://www.ifengus.com/api/cms/archives?cid=17&limite=10&apitoken=hiRNzRjQ!x2x@H@X")!
-       
-           URLSession.shared.dataTask(with: url) {(data,response,error) in
-               do {
-                   if let d = data {
-                       let decodedLists = try JSONDecoder().decode([Lists].self, from: d)
-                       DispatchQueue.main.async {
-                           self.lists = decodedLists
-                       }
-                   }else {
-                       print("No Data")
-                   }
-               } catch {
-                   print ("Error")
-               }
-               
-           }.resume()
-            
-}
+       let url = URL(string: "https://www.ifengus.com/api/cms/archives?cid=17&limite=10&apitoken=hiRNzRjQ!x2x@H@X")!
+       URLSession.shared.dataTask(with: url) {(data,response,error) in
+           do {
+            if let d = data {
+                let decodedLists = try JSONDecoder().decode(isresponsearchives.self, from: d)
+                if (decodedLists.code == 1){
+                    DispatchQueue.main.async {
+                        self.lists = decodedLists.data!
+                    }
+                } else {
+                    print("data build error")
+                }
+            } else {
+               print("No Data")
+            }
+           } catch {
+               print ("Error")
+           }
+       }.resume()
+    }
 }
 
